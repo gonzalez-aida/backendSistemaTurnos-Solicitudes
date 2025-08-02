bdd825b2c8f7ebd4ce00955de317f22902cfa532
gonzalez-aida <aidalu13oct@gmail.com>
2025-08-01 23:12:31 -0600

    Creación de backend Solicitudes

---
+++ src/main/java/mx/edu/uteq/backS/BackSApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.backS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/uteq/backS/BackSApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.backS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
